09d1a1c8c62b5451a8dbddf24383aa8ddb1fd7f3
Meenakshi Sundaram <vms20591@gmail.com>
2017-12-10 14:24:06 +0530

    Allowing Multiple Guesses with Looping

--- src/main.rs
+++ src/main.rs
@@ -16,38 +16,40 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("The secret number is: {}", secret_number);
+    
+    loop{
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        // Variables are immutable by default
+        // "mut" makes "guess" mutable
+        // "::" calls associated functions (static methods)
+        let mut guess = String::new();
 
-    // Variables are immutable by default
-    // "mut" makes "guess" mutable
-    // "::" calls associated functions (static methods)
-    let mut guess = String::new();
+        // "read_line" takes a "reference" where the input is stored
+        // "reference" should be "mutable", so we add "&mut guess" instead of "&guess"
+        // "reference" are immutable by default
+        io::stdin().read_line(&mut guess)
+        // "read_line" returns a "Result" which an "enum"
+        // There are multiple result types and result of "read_line"
+        // belongs to io::Result. It holds two variants "Ok" and "Err"
+        // If its "Ok", expect returns whatever "Ok" holds
+        // If its "Err", it crashes and prints the msg passed to it
+            .expect("Failed to read line");
 
-    // "read_line" takes a "reference" where the input is stored
-    // "reference" should be "mutable", so we add "&mut guess" instead of "&guess"
-    // "reference" are immutable by default
-    io::stdin().read_line(&mut guess)
-    // "read_line" returns a "Result" which an "enum"
-    // There are multiple result types and result of "read_line"
-    // belongs to io::Result. It holds two variants "Ok" and "Err"
-    // If its "Ok", expect returns whatever "Ok" holds
-    // If its "Err", it crashes and prints the msg passed to it
-        .expect("Failed to read line");
+        // "guess" is a shadow of original rather than different variable
+        // trim any space or new line 
+        // parse the input as number and in this case "u32"
+        // this makes rust infers "secret_number" as "u32"
+        let guess: u32 = guess.trim().parse()
+                            .expect("Please type a number!");
 
-    // "guess" is a shadow of original rather than different variable
-    // trim any space or new line 
-    // parse the input as number and in this case "u32"
-    // this makes rust infers "secret_number" as "u32"
-    let guess: u32 = guess.trim().parse()
-                        .expect("Please type a number!");
+        // Print formatted string where "{}" is the placeholder
+        println!("You guessed: {}", guess);
 
-    // Print formatted string where "{}" is the placeholder
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
